7f1aae160a03347cf32d93dfa83122d32854eda9
aohanhongzhi <aohanhongzhi@qq.com>
2022-03-20 16:53:55 +0800

    内存泄露的时候到处堆信息

--- asset/shell/restart.sh
+++ asset/shell/restart.sh
@@ -3,5 +3,5 @@ PID=$(cat /media/data/eric/app/print-user/print_server.pid)
 nohup kill -9 $PID
 export JAVA_HOME=/opt/eric/jbr/
 export PATH=$JAVA_HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
-nohup java -jar /media/data/eric/app/print-user/print-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod -Xmx1G -Xms512M -server -XX:+UseG1GC >>/media/data/eric/app/print-user/print.log  2>&1 &
+nohup java -jar /media/data/eric/app/print-user/print-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod  -Xmx1G -Xms512M -server -XX:+UseG1GC -XX:+HeapDumpOnOutOfMemoryError >>/media/data/eric/app/print-user/print.log  2>&1 &
 echo $! > /media/data/eric/app/print-user/print_server.pid
